9f999175b25f717b2f342e4b314f1afc098bfd1b
FranklinS734 <martinc3@usf.edu>
2025-02-23 00:20:59 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -1,5 +1,4 @@
 // Task 1: Creating a Product Class
-//Task 1 - Created Product Class
 
 class Product { //Creating class Product
     constructor(name, id, price, stock) { 
@@ -28,3 +27,29 @@ console.log(prod1.getDetails());
 prod1.updateStock(3);
 console.log(prod1.getDetails()); 
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+
+// Task 2: Creating an Order Class
+
+class Order { //Creating class Order
+    constructor(orderId, product, quantity) { 
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.product.updateStock(this.quantity);
+    };
+
+    getOrderDetails() { //Adding getOrderDetails()
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}"`; 
+        // returns order details
+    };
+};
+
+//Test cases
+const order1 = new Order(501, prod1, 2); 
+
+console.log(order1.getOrderDetails()); 
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
